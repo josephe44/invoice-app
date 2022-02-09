841c2fc2838798c5f14bbf2fd530caba6f02ede4
Osuohia Emmanuel <josephe442@gmail.com>
2022-02-09 17:10:08 +0100

    finish up controller, error handling and added express async handler

--- backend/controllers/invoiceController.js
+++ backend/controllers/invoiceController.js
@@ -1,31 +1,36 @@
+const asyncHandler = require('express-async-handler')
+
 //@desc Get Invoices
 //@route GET /api/invoices
 //@access Public
-const getInvoices = (req, res) => {
+const getInvoices = asyncHandler( async (req, res) => {
+  if (!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
   res.status(200).json({ message: 'Get Invoices' })
-}
+})
 
 //@desc Set Invoices
 //@route POST /api/invoice
 //@access Public
-const setInvoice = (req, res) => {
+const setInvoice = asyncHandler( async (req, res) => {
   res.status(200).json({ message: 'Add Invoice' })
-}
-
+})
 
 //@desc Update Invoices
 //@route PUT /api/invoices/:id
 //@access Public
-const updateInvoice = (req, res) => {
+const updateInvoice = asyncHandler( async (req, res) => {
   res.status(200).json({ message: `Update Invoice ${req.params.id}` })
-}
+})
 
 //@desc Delete Invoices
 //@route DELETE /api/invoices
 //@access Public
-const deleteInvoice = (req, res) => {
+const deleteInvoice = asyncHandler( async (req, res) => {
   res.status(200).json({ message: `Delete Invoice ${req.params.id}` })
-}
+})
 
 module.exports = {
   getInvoices,
