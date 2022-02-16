0626161dda37ecb0b363891a3836acf40b555cc5
Osuohia Emmanuel <josephe442@gmail.com>
2022-02-16 17:34:36 +0100

    Worked on logout, login reducer with redux and user login logic

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -28,7 +28,23 @@ export const register = createAsyncThunk(
     }
   }
 )
+//Logout user
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout()
+})
 
+//Login user
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+  try {
+    return await authService.login(user)
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -42,6 +58,7 @@ export const authSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
+      //Register user
       .addCase(register.pending, (state) => {
         state.isLoading = true
       })
@@ -54,6 +71,26 @@ export const authSlice = createSlice({
         state.isLoading = false
         state.isError = true
         state.message = action.payload
+        state.user = null
+      })
+      //Login user
+      .addCase(login.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        state.user = null
+      })
+
+      //Logout user
+      .addCase(logout.fulfilled, (state) => {
         state.user = null
       })
   },
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,10 @@
 import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+
 import { FaSignInAlt } from 'react-icons/fa'
 
 function Login() {
@@ -9,6 +15,23 @@ function Login() {
 
   const { email, password } = formData
 
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    if (isSuccess || user) {
+      navigate('/')
+    }
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -18,6 +41,16 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+    dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
